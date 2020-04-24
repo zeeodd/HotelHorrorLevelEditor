4a6fa31e2b1af52578ee50233016c74d706d40ae
zeeodd <evergreenskier@gmail.com>
2020-04-24 10:51:03 -0400

    Uploading Complete

--- scripts/d3-context-menu.js
+++ scripts/d3-context-menu.js
@@ -21,19 +21,26 @@ d3.contextMenu = function(menu, openCallback) {
         d.action(elm, data, index);
         if (i == 0) { // TILE
           elm.setAttribute("fill", "#59AEEB");
-        } else if (i == 1) { // EMPTY
+        }
+        else if (i == 1) { // EMPTY
           elm.setAttribute("fill", "#000000");
-        } else if (i == 2) { // PLAYER SPAWN
+        }
+        else if (i == 2) { // PLAYER SPAWN
           elm.setAttribute("fill", "#E4EEF5");
-        } else if (i == 3) { // INVADER SPAWN
+        }
+        else if (i == 3) { // INVADER SPAWN
           elm.setAttribute("fill", "#A9F5C5");
-        } else if (i == 4) { // BED
+        }
+        else if (i == 4) { // BED
           elm.setAttribute("fill", "#D32CDB");
-        } else if (i == 5) { // BED TILE
+        }
+        else if (i == 5) { // BED TILE
           elm.setAttribute("fill", "#CB79F7");
-        } else if (i == 6) { // CLOSET
+        }
+        else if (i == 6) { // CLOSET
           elm.setAttribute("fill", "#ECA025");
-        } else if (i == 7) { // DRESSER
+        } 
+        else if (i == 7) { // DRESSER
           elm.setAttribute("fill", "#F06086");
         }
         d3.select(".d3-context-menu").style("display", "none");
